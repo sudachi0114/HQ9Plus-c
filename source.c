04c6be44244050d92f45fd46570f8d45f9a68cbc
sudachi0114 <jnevariste@gmail.com>
2023-01-01 19:57:10 +0900

    自分のファイルの中身を標準出力に出力する

--- source.c
+++ source.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-    printf("%s\n", __FILE__);
+    FILE * fp = NULL;
+    fp = fopen(__FILE__, "r");
 
+    int getf;
+
+    while (1)
+    {
+        getf = fgetc(fp);
+        if ( !feof(fp) ) fputc(getf, stdout);
+        else break;
+    }
+
+    fclose(fp);
     return 0;
 }
