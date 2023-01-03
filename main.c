c364067b00664ae271cb29b852918a45fe83bc12
sudachi0114 <jnevariste@gmail.com>
2023-01-03 12:13:16 +0900

    ソースコード or ソースファイル名を受け取る処理を追加

--- main.c
+++ main.c
@@ -5,7 +5,19 @@
 #include "99.c"
 #include "counter.c"
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    char s[16];
+
+    if (argv[1] != NULL) {
+        // ファイルで渡された場合
+        printf("argv[1]: %s\n", argv[1]);
+    } else {
+        // 直にソースコードを渡された場合
+        scanf("%s", s);
+        printf("%s\n", s);
+    }
+
+    /*
     hello();
 
     print_source();
@@ -14,6 +26,7 @@ int main(void) {
 
     int n = 0;
     increment(n);
+    */
 
     return 0;
 }
