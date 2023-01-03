5d239d1034fd692b07b11500612b3396b4d7d183
sudachi0114 <jnevariste@gmail.com>
2023-01-03 16:17:07 +0900

    ソースコードを読み込む処理を追加

--- main.c
+++ main.c
@@ -6,11 +6,20 @@
 #include "counter.c"
 
 int main(int argc, char *argv[]) {
-    char s[16];
+    char s[] = "";
+    FILE * fp = NULL;
 
     if (argv[1] != NULL) {
-        // ファイルで渡された場合
-        printf("argv[1]: %s\n", argv[1]);
+        /* ファイルで渡された場合
+         * ファイルを開いてソースコードを読み込む
+         */
+        fp = fopen(argv[1], "r");
+
+        fscanf(fp, "%s", &s);
+
+        fclose(fp);
+
+        printf("%s\n", s);
     } else {
         // 直にソースコードを渡された場合
         scanf("%s", s);
