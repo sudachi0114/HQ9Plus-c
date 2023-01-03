94b9715d36bdf9195a6fe1008414c6e1ed1165f1
sudachi0114 <jnevariste@gmail.com>
2023-01-03 12:01:06 +0900

    counter 機能を追加

--- counter.c
+++ counter.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+int increment(int n) {
+    return n+=1;
+}
+
+/*
 int main(void) {
     int n = 0;
+    n = increment(increment(n));
+
     printf("%d\n", n);
-    n++;
-    n++;
-    printf("%d\n", n);
+
     return 0;
 }
+*/
--- main.c
+++ main.c
@@ -2,11 +2,15 @@
 
 #include "hello.c"
 #include "99.c"
+#include "counter.c"
 
 int main(void) {
     hello();
 
     print_99_bottle_of_beer();
+
+    int n = 0;
+    increment(n);
 
     return 0;
 }
